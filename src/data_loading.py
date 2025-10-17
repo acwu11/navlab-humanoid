b8109c15d018369308fcb2a47a079b9456ea9f73
Asta Wu <astawu@stanford.edu>
2025-10-17 11:43:25 -0700

    adding a gitignore

--- src/data_loading.py
+++ src/data_loading.py
@@ -9,4 +9,4 @@ SLAT_PLY_DIR_NAME = "splat_plys"
 
 BLUE_PRINT_PATH = os.path.join(SRC_DIR, BLUEPRINTS_DIR_NAME)
 SLAT_PLY_PATH = os.path.join(SRC_DIR, SLAT_PLY_DIR_NAME)
-# -----------------------------------------------------#
+# ----------------------------------------------------------#
