ce65ea6cdec39a88c1a55676746757cd69342b22
Asta Wu <astawu@stanford.edu>
2025-10-17 11:31:56 -0700

    data pathing from shared workstation folder

---
+++ src/data_loading.py
@@ -0,0 +1,12 @@
+"""Scripts to load raw data from shared folder."""
+
+import os
+
+# ----------------------- Data  Paths -----------------------#
+SRC_DIR = "/home/shared/data_raw/SRC/"
+BLUEPRINTS_DIR_NAME = "polycam_blueprints"
+SLAT_PLY_DIR_NAME = "splat_plys"
+
+BLUE_PRINT_PATH = os.path.join(SRC_DIR, BLUEPRINTS_DIR_NAME)
+SLAT_PLY_PATH = os.path.join(SRC_DIR, SLAT_PLY_DIR_NAME)
+# -----------------------------------------------------#
